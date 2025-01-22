18aa57472b44f7c5e6651b7ecfe619f137309007
jhw2167 <jhw2167@columbia.edu>
2025-01-21 20:44:19 -0600

    Removed and modified loader service files - Updated version keyword to use mod_version

--- common/src/main/java/com/holybuckets/foundation/Constants.java
+++ common/src/main/java/com/holybuckets/foundation/Constants.java
@@ -6,6 +6,6 @@ import org.slf4j.LoggerFactory;
 public class Constants {
 
 	public static final String MOD_ID = "hbs_foundation";
-	public static final String MOD_NAME = "HB's Foundation";
+	public static final String MOD_NAME = "HBs Foundation";
 	public static final Logger LOG = LoggerFactory.getLogger(MOD_NAME);
 }
